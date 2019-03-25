dcfad3d4f62d0382a8e2db4f187c3335548f0ca9
daniel <835168932@qq.com>
2019-03-25 17:56:24 +0800

    add a sample project

---
+++ src/main/java/cn/edu/sjtu/sfino/softwareinnovation/SoftwareinnovationApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.sjtu.sfino.softwareinnovation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareinnovationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftwareinnovationApplication.class, args);
+    }
+
+}

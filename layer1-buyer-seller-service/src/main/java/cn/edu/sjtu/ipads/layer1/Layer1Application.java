64983cb59c935cb7ee3820e127d2b0ccd23f7d8a
daniel <835168932@qq.com>
2019-06-27 20:30:59 +0800

    little modify

--- layer1-buyer-seller-service/src/main/java/cn/edu/sjtu/ipads/layer1/Layer1Application.java
+++ layer1-buyer-seller-service/src/main/java/cn/edu/sjtu/ipads/layer1/Layer1Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +21,7 @@ import java.io.IOException;
 @SpringBootApplication
 @EnableSwagger2
 @RestController
+@RefreshScope
 public class Layer1Application {
 
     @Value("${key}")

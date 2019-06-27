d6d269e94b316c69b5dc5b07348e958ee07994d4
daniel <835168932@qq.com>
2019-06-27 19:59:09 +0800

    little modify

--- layer1-buyer-seller-service/src/main/java/cn/edu/sjtu/ipads/layer1/Swagger2.java
+++ layer1-buyer-seller-service/src/main/java/cn/edu/sjtu/ipads/layer1/Swagger2.java
@@ -21,7 +21,7 @@ public class Swagger2 {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("cn.edu.sjtu.ipads"))
+                .apis(RequestHandlerSelectors.basePackage("cn.edu.sjtu.ipads.layer1"))
                 .paths(PathSelectors.any())
                 .build();
     }
